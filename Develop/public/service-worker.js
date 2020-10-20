edbc6904e4fbcbf0c200399b489a8528651999d4
Daniel.Yu <adam741963@gmail.com>
2020-10-20 16:13:13 -0700

    activate in service worker

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -26,6 +26,26 @@ self.addEventListener('install', function(evt) {
 });
 
 
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  const currentCaches = [CACHE_NAME, DATA_NAME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fatch', event => {
   if(event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
